9eca4914cd85c8ed55e85726e0bc7db103a12e16
Moritz Eck <moritz.eck@gmail.com>
2019-05-06 14:17:12 +0200

    update to annotate_images function - print/output

--- landmark_annotation/annotate.py
+++ landmark_annotation/annotate.py
@@ -5,17 +5,18 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from skimage import io
 
-def annotate_images(pathname):
+def annotate_images(pathname, save_to_file=True, print_preview=True):
     """
     This method annotates the facial landmarks of all images in the folder 'pathname'
     and creates a matrix containing the landmarks one row per matrix.
 
     Inputs:
-    - pathname: path to folder
+    - pathname: string - path to folder
+    - save_to_file: bool - saving the result to a CSV file
+    - print_preview: bool - print a preview of the dataset
     
     Returns:
-    - df: pandas dataframe containing all landmarks
-    - output: csv file
+    - df: pandas dataframe containing all landmarks (one row per image)
     """
     # Run the 3D face alignment on a test image, without CUDA.
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, device='cuda', flip_input=True)
@@ -46,11 +47,16 @@ def annotate_images(pathname):
     
     # create pandas dataframe out of it
     df = pd.DataFrame.from_dict(data)
-    print(df.head())
 
-    # write result to CSV
-    filename = "{}/output.csv".format(pathname)
-    df.to_csv(filename, sep=",", header=True, index=False)
+    if print_preview:
+        print(df.head())
+
+    if save_to_file:
+        # write result to CSV
+        filename = "{}/image_landmarks.csv".format(pathname)
+        df.to_csv(filename, sep=",", header=True, index=False)
+
+    return df
         
     
 if __name__ == "__main__":
