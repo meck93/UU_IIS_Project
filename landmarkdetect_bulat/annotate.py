9fff832e09f679905fe1ced04d43637f0194faca
Moritz Eck <moritz.eck@gmail.com>
2019-05-15 16:48:55 +0200

    modified: annotate dataset method to include only relevant columns/landmarks

--- landmarkdetect_bulat/annotate.py
+++ landmarkdetect_bulat/annotate.py
@@ -4,6 +4,8 @@ import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from skimage import io
+from constants import LANDMARK_MAPPING
+
 
 def annotate_images(pathname, save_to_file=True, print_preview=True):
     """
@@ -14,14 +16,14 @@ def annotate_images(pathname, save_to_file=True, print_preview=True):
     - pathname: string - path to folder
     - save_to_file: bool - saving the result to a CSV file
     - print_preview: bool - print a preview of the dataset
-    
+
     Returns:
     - df: pandas dataframe containing all landmarks (one row per image)
     """
     # Run the 3D face alignment on a test image, without CUDA.
     fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, device='cuda', flip_input=True)
 
-    # get the facial landmarks  
+    # get the facial landmarks
     landmarks = fa.get_landmarks_from_directory(pathname)
 
     # create dataframe to store landmark coordinates
@@ -29,8 +31,8 @@ def annotate_images(pathname, save_to_file=True, print_preview=True):
 
     # column names C1-C68 + key: label
     n_columns = len(list(landmarks.values())[0][0])
-    columns = ["C{}".format(i+1) for i in range(n_columns)]
-    columns.append("label")
+    columns = ["{}".format(i+1) for i in range(n_columns)]
+    columns.append("filename")
 
     # initialize dictionary
     data = {key: list() for key in columns}
@@ -38,26 +40,37 @@ def annotate_images(pathname, save_to_file=True, print_preview=True):
     for key, values in landmarks.items():
         # extract the picture name to use it as label
         label = key[key.rfind("\\")+1:key.rfind(".")]
-        data['label'].append(label)
+        data['filename'].append(label)
 
         for i in range(len(values[0])):
             # fix assingment
-            data["C{}".format(i+1)].append(values[0][i])
+            data["{}".format(i+1)].append(values[0][i])
 
-    
     # create pandas dataframe out of it
     df = pd.DataFrame.from_dict(data)
+
+    # extract the relevant columns and drop the irrelevant
+    relevant_columns = list(LANDMARK_MAPPING.values())
+    relevant_columns = [col for col in relevant_columns if col is not None]
+    not_relevant_columns = np.arange(1, 69, 1)
+    not_relevant_columns = [str(col) for col in not_relevant_columns if col not in relevant_columns]
+    df = df.drop(columns=not_relevant_columns)
+
+    # rename the columns with the original names (key&value inverted)
+    REVERSED_LANDMARK_MAPPING = {str(value): key for key, value in LANDMARK_MAPPING.items()}
+    df = df.rename(columns=REVERSED_LANDMARK_MAPPING)
 
     if print_preview:
         print(df.head())
 
     if save_to_file:
         # write result to CSV
-        filename = "{}/image_landmarks.csv".format(pathname)
+        filename = "./image_landmarks_dataset3.csv"
         df.to_csv(filename, sep=",", header=True, index=False)
 
     return df
-        
-    
+
+
 if __name__ == "__main__":
-    annotate_images(pathname="../datasets/self_created/test/input/")
+    annotate_images(pathname="../datasets/self_created/dataset3/rgb/")
+    # annotate_images(pathname="../datasets/test_annotation/input/")
