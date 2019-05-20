7ee8d36cce7945ef3bb8c31de8305334038862e6
Lukas Mehl <lukas.mehl@gmail.com>
2019-05-20 11:04:05 +0200

    add face cropping

--- faceDetection.py
+++ faceDetection.py
@@ -62,17 +62,25 @@ def main(rgb_file, depth_file):
     cv2.destroyAllWindows()
 
 
-def detectFaces(image, depth):
+def detectFaces(image, depth, boundary=(-0.05, 0.15)):
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     face_cascade = cv2.CascadeClassifier(cv2.__path__[0] + '/data/haarcascade_frontalface_default.xml')
     faces = face_cascade.detectMultiScale(gray_image, 1.3, 5)
 
     output = []
+    im_h, im_w = gray_image.shape
+
     if faces is not None:
         for (x, y, w, h) in faces:
-            d = depth[y:y+h, x:x+w]
-            im = gray_image[y:y+h, x:x+w]
+            b_x = int(round(boundary[0] * w))
+            b_y = int(round(boundary[1] * h))
+            x1 = min(max(x - b_x, 0), im_w)
+            y1 = min(max(y - b_y, 0), im_h)
+            x2 = min(max(x + w + b_x, 0), im_w)
+            y2 = min(max(y + h + b_y, 0), im_h)
+            d = depth[y1:y2, x1:x2]
+            im = gray_image[y1:y2, x1:x2]
 
             d = cv2.resize(d, (128, 128))
             d = np.asarray(d, dtype='float')
