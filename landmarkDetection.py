af881fc812b9055cccfc840d3ab88645eb5a31fc
Moritz Eck <moritz.eck@gmail.com>
2019-05-21 16:48:08 +0200

    changed: custom loss function import in LandmarkDetector

--- landmarkDetection.py
+++ landmarkDetection.py
@@ -1,9 +1,11 @@
 from keras.models import load_model
-from buildModel import avg_l2_dist
+from buildModel import avg_l2_dist, avg_l1_dist
 
 class LandmarkDetector():
     def __init__(self, modelpath="./network.hdf5"):
-        self.model = load_model(modelpath, custom_objects={'avg_dist_coordinates': avg_l2_dist})
+        self.l1_loss = {"avg_l1_dist": avg_l1_dist}
+        self.l2_loss = {"avg_l2_dist": avg_l2_dist}
+        self.model = load_model(modelpath, custom_objects=self.l2_loss)
 
     def detectLandmarks(self, input_vector):
         if input_vector.shape != (1,2,128,128):
